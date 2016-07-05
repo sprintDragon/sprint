1997937734776ddf0f9fa3ea68f6c8220f573215
psiitoy <psiitoy@126.com>
2016-07-05 16:57:11 +0800

    增加测试用例

--- trace/src/test/java/com/sprint/trace/TestDemoBiz.java
+++ trace/src/test/java/com/sprint/trace/TestDemoBiz.java
@@ -7,6 +7,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by wangdi on 16-7-4.
@@ -21,5 +25,36 @@ public class TestDemoBiz {
     @Test
     public void testDemo() throws Exception {
         iDemoBiz.queryOrder(22l, 11l);
+    }
+
+    volatile boolean lock = true;
+
+    @Test
+    public void testMuti() throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        long nowTime = System.nanoTime();
+        long num = 0;
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(20000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                lock = false;
+            }
+        });
+        while (lock) {
+            Future<String> f = executorService.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    return iDemoBiz.queryOrder(System.currentTimeMillis(), 11l);
+                }
+            });
+            num++;
+            Thread.sleep(1);
+        }
+        System.out.println("时间消耗 : " + (System.nanoTime() - nowTime) + ",方法执行条数：" + num);
     }
 }
